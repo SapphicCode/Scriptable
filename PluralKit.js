f24438ea8113b2d0467319e8bca068c39b5cb1e0
Cassandra <cassandra+git@services.beelen.one>
2020-10-20 13:12:06 +0200

    Refactor a lot and add error-handling

--- PluralKit.js
+++ PluralKit.js
@@ -7,51 +7,76 @@ async function getSystemData(systemID) {
   let systemURL = "https://api.pluralkit.me/v1/s/" + systemID
   let frontersURL = systemURL + "/fronters"
 
-  let data = {
-    meta: await new Request(systemURL).loadJSON(),
-    fronters: (await new Request(frontersURL).loadJSON()).members,
+  let system = {}
+
+  // fetching metadata
+  try {
+    system.meta = await new Request(systemURL).loadJSON()
+  } catch {
+    return null
   }
-  return data
+
+  // fetching fronters
+  // this may fail due to privacy reasons, no logged switches, etc
+  try {
+    let fronters = await new Request(frontersURL).loadJSON()
+    system.fronters = fronters.members
+  } catch {
+    system.fronters = []
+  }
+
+  return system
+}
+
+async function setWidget(widget, systemID) {
+  let system = await getSystemData(args.widgetParameter)
+  if (system == null) {
+    widget.addText("PluralKit request failed, double-check the system ID")
+    return
+  }
+
+  if (system.fronters.length == 0) {
+    widget.backgroundImage = await new Request(
+      system.meta.avatar_url
+    ).loadImage()
+    let name = widget.addText(system.meta.name)
+    name.font = Font.lightSystemFont(14)
+    name.textColor = textColor
+    name.rightAlignText()
+    name.shadowRadius = 1
+
+    widget.addSpacer(null)
+  } else {
+    front = system.fronters[0]
+
+    widget.backgroundImage = await new Request(
+      front.avatar_url || system.meta.avatar_url
+    ).loadImage()
+
+    let name = widget.addText(front.name)
+    name.font = Font.systemFont(16)
+    name.textColor = textColor
+    name.rightAlignText()
+    name.shadowRadius = 1
+
+    widget.addSpacer(null)
+
+    let systemName = widget.addText(system.meta.name)
+    systemName.font = Font.lightSystemFont(14)
+    systemName.textColor = textColor
+    systemName.rightAlignText()
+    systemName.shadowRadius = 1
+  }
 }
 
 if (config.runsInWidget) {
-  let w = new ListWidget()
+  let widget = new ListWidget()
 
   if (!args.widgetParameter) {
-    w.addText("Please add a system ID to check, as parameter")
+    widget.addText("Please add a system ID to check, as parameter")
   } else {
-    let system = await getSystemData(args.widgetParameter)
-
-    if (system.fronters.length == 0) {
-      w.backgroundImage = await new Request(system.meta.avatar_url).loadImage()
-      let name = w.addText(system.meta.name)
-      name.font = Font.lightSystemFont(14)
-      name.textColor = textColor
-      name.rightAlignText()
-      name.shadowRadius = 1
-
-      w.addSpacer(null)
-    } else {
-      front = system.fronters[0]
-
-      w.backgroundImage = await new Request(
-        front.avatar_url || system.meta.avatar_url
-      ).loadImage()
-
-      let name = w.addText(front.name)
-      name.font = Font.systemFont(16)
-      name.textColor = textColor
-      name.rightAlignText()
-      name.shadowRadius = 1
-
-      w.addSpacer(null)
-
-      let systemName = w.addText(system.meta.name)
-      systemName.font = Font.lightSystemFont(14)
-      systemName.textColor = textColor
-      systemName.rightAlignText()
-      systemName.shadowRadius = 1
-    }
+    await setWidget(widget, args.widgetParameter)
   }
-  Script.setWidget(w)
+
+  Script.setWidget(widget)
 }
